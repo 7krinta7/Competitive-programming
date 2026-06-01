dc3632314b91175c8792fe39416be4daba651685
Krish <krishkrm07@gmail.com>
2026-06-01 11:07:22 +0530

    added 1845a

---
+++ Codeforces/1845A.cpp
@@ -0,0 +1,49 @@
+//1845A - Forbidden Integer
+//https://codeforces.com/problemset/problem/1845/A
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int k;
+    cin>>k;
+    while(k--){
+        int n,k,x;
+        cin>>n>>k>>x;
+        if(k==1&&x==1)
+        cout<<"NO"<<endl;
+        else{
+        if(x!=1){
+            cout<<"YES"<<endl;
+            cout<<n<<endl;
+            for(int i=0;i<n;i++)
+            cout<<1<<" ";
+            cout<<endl;
+        }
+        else{
+            if(n%2==0){
+                cout<<"YES"<<endl;
+                cout<<n/2<<endl;
+                for(int i=0;i<n/2;i++)
+                cout<<2<<" ";
+                cout<<endl;
+            }
+            else{
+                if(n==1)
+                cout<<"NO"<<endl;
+                else{
+                    if(k<3)
+                    cout<<"NO"<<endl;
+                    else{
+                        cout<<"YES"<<endl;
+                        cout<<n/2<<endl;
+                        for(int i=0;i<n/2-1;i++)
+                        cout<<2<<" ";
+                        cout<<3<<endl;
+                    }
+                }
+            }
+        }
+        }
+    }
+}
